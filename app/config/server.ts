fd4150b6d53fb461c69e7dd4fc62e1682233fec9
yueguoying <136574607+yueguoying@users.noreply.github.com>
2023-08-25 12:15:20 +0800

    Update server.ts
    多Key

--- app/config/server.ts
+++ app/config/server.ts
@@ -37,8 +37,13 @@ export const getServerSideConfig = () => {
     );
   }
 
+  // 增加Key池实现多Key 轮询
+// 从这里开始
+  const apiKeys = (process.env.OPENAI_API_KEY ?? '').split(',')
+  const apiKey = apiKeys.at(Math.floor(Math.random() * apiKeys.length)) ?? ''
   return {
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey, 
+// 到这结束
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
